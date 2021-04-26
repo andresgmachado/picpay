846fe8d88bc5553f92afab5c126dc8698e258407
andré Machado <andre@hiper.com.br>
2021-04-26 18:25:18 -0300

    ajuste botão

--- src/components/paybutton/index.js
+++ src/components/paybutton/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {MaterialIcons} from '@expo/vector-icons'
+import {Button, Label} from './styles';
+import {TouchableWithoutFeedback} from 'react-native';
 
-export default function Paybutton(){
-    return <Text style={{color: '#fff'}}>PAGAR</Text>
+export default function Paybutton( {onPress, focused}){
+    return (
+    <TouchableWithoutFeedback onPress={onPress}>
+        <Button
+          colors={
+              focused
+              ?['#fff', '#fff']
+              : ['#00fc6c','#00ac4a']
+          }
+            start={[1,0.2]}
+        >
+            <MaterialIcons
+                name='attach-money' 
+                size={30} 
+                color={ focused ? '#000' : '#fff'}
+            />
+        <Label focused={focused}>Pagar</Label>
+        </Button>
+        </TouchableWithoutFeedback>
+    );
 }
